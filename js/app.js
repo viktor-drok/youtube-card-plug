fe91e396b34d2f8d4b9ecc3275dd3a37a13b730a
viktor-drok <vityadrok@gmail.com>
2022-10-29 15:22:02 +0300

    first commit

---
+++ js/app.js
@@ -0,0 +1,17 @@
+
+let s1 = new Swiper(".s-1", {
+    slidesPerView: 4,
+    spaceBetween: 5,
+});
+
+let s2 = new Swiper(".s-2", {
+    slidesPerView: 4,
+});
+
+let s3 = new Swiper(".s-3", {
+    slidesPerView: 4,
+});
+
+let s4 = new Swiper(".s-4", {
+    slidesPerView: 4,
+});
